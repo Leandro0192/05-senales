b8d63749d93cf31f8aefc6ef8879f8385abbee08
leandro.e.ochoa@gmail.com <leandro.e.ochoa@gmail.com>
2022-05-04 14:18:31 -0300

    v0.0

---
+++ sig01.c
@@ -0,0 +1,15 @@
+/* Ejercicio 1*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+int main ()
+{
+   sleep(5);
+   
+   exit(5);}
